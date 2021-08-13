391bac9c7e21a9880589a2f28169c3c9ecfd630e
essential2189 <essential2189@gmail.com>
2021-08-13 23:56:28 +0900

    solved.ac

--- solved.ac/Silver IV/1002.py
+++ solved.ac/Silver IV/1002.py
@@ -1,12 +1,20 @@
 def solution(x1, y1, r1, x2, y2, r2):
     dis = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
 
+    # if 순서 중요
+    # 두 원이 일치하는 경우
     if dis == 0 and r1 == r2 :
         answer = -1
+
+    # 두 원이 한점에서 만나는 경우(내적, 외적)
     elif dis == r1+r2 or abs(r1-r2) == dis :
         answer = 1
+
+    # 두 원이 두점에서 만나는 경우
     elif abs(r1-r2) < dis < r1+r2 :
         answer = 2
+
+    # 두 원이 만나지 않는 경우
     else :
         answer = 0
 
