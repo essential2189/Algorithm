05d81a751e533c97ca9b3bc80c86595e38358fd7
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-05-16 17:14:55 +0900

    최빈 구하기

--- sw_expert_academy/D2/1204.py
+++ sw_expert_academy/D2/1204.py
@@ -1,5 +1,16 @@
+from sys import stdin
+from collections import Counter
+
 def solution():
     T = int(input())
+
+    for _ in range(T):
+        tc_num = int(input())
+        score = stdin.readline().split(" ")
+        score_int = [int(i) for i in score[0:-1]]
+        counter = Counter(score_int).most_common(1)
+
+        print("#{} {}".format(tc_num, counter[0][0]))
 
 
 
