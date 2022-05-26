8698ff651363e19e5c3a4a12b3f76cdaf4045ace
essential2189 <essential2189@gmail.com>
2022-05-26 23:07:35 +0900

    홀수만 더하기

--- sw_expert_academy/D1/2072.py
+++ sw_expert_academy/D1/2072.py
@@ -1,29 +1,10 @@
 def solution():
-    date = input()
-    answer = ""
-
-    _31 = [1, 3, 5, 7, 8, 10, 12]
-    _30 = [4, 6, 9, 11]
-
-    year = date[:4]
-    month = date[4:6]
-    day = date[6:]
-
-    answer += year+"/"
-
-    if 1 <= int(month) <= 12:
-        answer += month + "/"
-    else:
-        return -1
-
-    if int(month) in _31 and 1 <= int(day) <= 31:
-        answer += day
-    elif int(month) in _30 and 1 <= int(day) <= 30:
-        answer += day
-    elif int(month) == 2 and 1 <= int(day) <= 28:
-        answer += day
-    else:
-        return -1
+    nums = input().split()
+    nums_int = [int(i) for i in nums]
+    answer = 0
+    for n in nums_int:
+        if n % 2 != 0:
+            answer += n
 
     return answer
 
