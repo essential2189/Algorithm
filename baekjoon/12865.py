82b4087c4fda5d8dc04db323bcc1aedd1690b040
essential2189 <essential2189@gmail.com>
2023-01-16 22:09:26 +0900

    평범한 배낭

--- baekjoon/12865.py
+++ baekjoon/12865.py
@@ -1,11 +1,25 @@
 def solution(items):
+    for i in range(1, N+1):
+        for j in range(1, K+1):
+            w, v = items[i]
+            print(w, v)
 
+            if j < w:
+                bag[i][j] = bag[i-1][j]
+                print(bag)
+            else:
+                bag[i][j] = max(v + bag[i-1][j-w], bag[i-1][j])
+                print(bag)
+
+    return bag[N][K]
 
 N, K = map(int, input().split(" "))
 
-items = []
+bag = [[0 for _ in range(K+1)] for _ in range(N+1)]
+items = [[0,0]]
 for _ in range(N):
     W, V = map(int, input().split(" "))
     items.append([W, V])
 
-solution(items)
+
+print(solution(items))
