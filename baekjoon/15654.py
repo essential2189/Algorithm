58e3a14b9c8c501cf736d9ae70325356ccfe903d
essential2189 <essential2189@gmail.com>
2023-01-18 22:22:04 +0900

    N과 M(5) (백트래킹)

---
+++ baekjoon/15654.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+
+nums = list(map(int, input().split()))
+nums.sort()
+
+s = []
+
+def backtracking(depth):
+    if depth == M:
+        print(" ".join(map(str, s)))
+        return
+
+    for i in range(N):
+        if nums[i] in s:
+            continue
+        s.append(nums[i])
+        backtracking(depth+1)
+        s.pop()
+
+backtracking(0)
