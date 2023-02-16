ac462a32b67530f5fc118a6c5afc31395268ce02
왕승재 <70889358+essential2189@users.noreply.github.com>
2023-02-16 23:47:32 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,21 +1,19 @@
 N, K = map(int, input().split())
 
-country = []
+country_list = []
 for _ in range(N):
-    country.append(list(map(int, input().split())))
+    country_list.append(list(map(int, input().split())))
 
 country_dict = {}
-for c in country:
-    country_dict[c[0]] = c[1:]
+for c in country_list:
+    if str(c[1:]) not in country_dict:
+        country_dict[str(c[1:])] = []
+    country_dict[str(c[1:])].append(c[0])
 
-country_dict = sorted(country_dict.items(), key=lambda item: item[1], reverse=True)
+country_dict = sorted(country_dict.items(), key=lambda item: list(item[0]), reverse=True)
 
-last = []
 for idx, cd in enumerate(country_dict):
-    if cd[0] == K:
-        if last == cd[1:]:
-            print(idx)
+    for country in cd[1]:
+        if country == K:
+            print(idx+1)
             break
-        print(idx + 1)
-        break
-    last = cd[1:]
