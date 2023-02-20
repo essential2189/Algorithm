59b6d64aaf2040d6f8a9a68e56cfdb883017f4f6
왕승재 <70889358+essential2189@users.noreply.github.com>
2023-02-20 23:02:21 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 232 ms, Memory: 125112 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -0,0 +1,21 @@
+import math
+
+def solution(person, game):
+    if game == "Y":
+        return len(person)
+
+    if game == "F":
+        return len(person) // 2
+
+    if game == "O":
+        return len(person) // 3
+
+
+n, game = input().split()
+
+person = set()
+for _ in range(int(n)):
+    person.add(input())
+
+
+print(solution(person, game))
