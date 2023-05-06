bcc4a2dcfd69b2dbd25c86df26eeda7a677d2940
왕승재 <essential2189@gmail.com>
2023-05-06 19:08:57 +0900

    [level 3] Title: 등굣길, Time: 2.53 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 def solution(m, n, puddles):
     dp = [[0] * (m + 1) for _ in range(n + 1)]
     dp[1][1] = 1
@@ -9,7 +7,8 @@ def solution(m, n, puddles):
             if [j, i] in puddles:
                 dp[i][j] = 0
                 continue
+                
             
             dp[i][j] += (dp[i-1][j] + dp[i][j-1]) % 1000000007
-    
+
     return dp[n][m]
