9f3f91be37d4494a251a092507b822ba21166a73
왕승재 <essential2189@gmail.com>
2023-05-13 00:45:07 +0900

    [Gold V] Title: 수강 과목, Time: 332 ms, Memory: 189536 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.py
@@ -0,0 +1,22 @@
+def solution(N, K, times, grades):
+    dp = [[0] * (n + 1) for _ in range(k + 1)]
+
+    for lecture_num in range(1, k+1):
+        for now_time in range(1, n+1):
+            if time[lecture_num-1] > now_time:
+                dp[lecture_num][now_time] = dp[lecture_num-1][now_time]
+            else:
+                dp[lecture_num][now_time] = max(important[lecture_num-1] + dp[lecture_num-1][now_time - time[lecture_num-1]], dp[lecture_num-1][now_time])
+                
+    return dp[K][N]
+
+n, k = map(int,input().split())
+time = []
+important = []
+for _ in range(k):
+    i, t = map(int,input().split())
+    time.append(t)
+    important.append(i)
+
+print(solution(n, k, time, important))
+
