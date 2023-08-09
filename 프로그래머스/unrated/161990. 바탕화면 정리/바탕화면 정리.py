cb377ba279ada3f863d9be53265c4a9c1251729e
왕승재 <essential2189@gmail.com>
2023-08-10 01:50:27 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
@@ -0,0 +1,13 @@
+def solution(wallpaper):
+    x = []
+    y = []
+    
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == "#":
+                y.append(i)
+                x.append(j)
+    
+    answer = [min(y), min(x), max(y)+1, max(x)+1]
+    
+    return answer
